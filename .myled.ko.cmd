a0f5d12877dede8aaf381017f494c546e814a74c
YugoNishio <71488443+YugoNishio@users.noreply.github.com>
2021-12-12 20:28:05 +0900

    Delete .myled.ko.cmd

--- .myled.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ubuntu/robot_system/myled.ko := ld -r  -EL  -maarch64elf  --build-id  -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds -o /home/ubuntu/robot_system/myled.ko /home/ubuntu/robot_system/myled.o /home/ubuntu/robot_system/myled.mod.o;  true
